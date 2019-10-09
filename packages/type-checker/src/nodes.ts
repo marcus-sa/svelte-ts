ce4ffe69a8014c99f29425c5aa6c4a6b486852e6
Marcus S. Abildskov <marcus-sa@outlook.com>
2019-10-09 17:24:42 +0200

    feat(type-checker): re-export nodes from svelte

---
+++ packages/type-checker/src/nodes.ts
@@ -0,0 +1,5 @@
+import InlineComponent from 'svelte/types/compiler/compile/nodes/InlineComponent';
+import IfBlock from 'svelte/types/compiler/compile/nodes/IfBlock';
+import Attribute from 'svelte/types/compiler/compile/nodes/Attribute';
+
+export { InlineComponent, IfBlock, Attribute };
